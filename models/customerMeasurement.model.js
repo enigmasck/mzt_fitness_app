8bd9de130772a3e4c3d9c680460947dc000587f8
manonbaillon <baillon.manon@gmail.com>
2019-10-11 18:09:09 +0200

    Added MongoDB connection successful + dependencies

---
+++ models/customerMeasurement.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const CustomerSchema = mongoose.Schema({
+	measurementId: String,
+	measurement_date: String,
+	weight: Number,
+        height: Number,
+        body_left_arm: Number,
+        body_right_arm: Number,
+        body_left_leg: Number,
+        body_right_leg: Number,
+        body_neck: Number,
+        body_hip: Number,
+        body_right_leg_thigh: Number,
+        body_left_leg_thigh: Number,
+        dickson_metric: Number
+}, {
+	timestamp: true
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema)
---
+++ models/exercise.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const ExerciseSchema = mongoose.Schema({
+	name: String,
+        description: String,
+        equipement_required: String,
+        exercise_type: String,
+        muscles_targeted: String,
+        set_break: Number,
+        repetition: Number,
+        sets: Number,
+        exercise_est_duration: Number,   
+        exercise_tag: Array
+}, {
+	timestamp: true
+});
+
+module.exports = mongoose.model('Exercise', ExerciseSchema)
