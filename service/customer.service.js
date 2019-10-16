9bb390b4e809bb3445dd72201e38f9db46ef2ffb
Timothy Sanders <timothyasanders@gmail.com>
2019-10-16 13:52:23 +0200

    Reworking Customer controller as customer service.

--- service/customer.service.js
+++ service/customer.service.js
@@ -27,94 +27,82 @@ exports.findOne = function(custId) {
 };
 
 // Create and save a new customer
-exports.create = function() {
+exports.create = function(cust) {
   return new Promise(function(resolve, reject) {
         // Validate request
-        if(!req.body.content) {
+        if(!cust) {
             reject('Customer content can not be empty');
         }
 
         // Create a customer
         const customer = new Customer({ 
-            first_name: req.body.first_name || "NA",
-            last_name: req.body.last_name || "NA",
-            signup_date: req.body.signup_date || "NA",
-            gender: req.body.gender || "NA",
-            dob: req.body.dob || "NA",
-            activity_level: req.body.activity_level || "NA",
-            goal: req.body.goal || "NA",
-            phone: req.body.phone || "NA",
-            email: req.body.email || "NA",
-            address: req.body.address || "NA",
-            health_condition: req.body.health_condition || "NA",
-            occupation: req.body.occupation || "NA",
-            availability: req.body.availability || "NA",
-            commitment: req.body.commitment || "NA"
+            first_name: cust.firstName || "NA",
+            last_name: cust.lastName || "NA",
+            signup_date: cust.signupDate || "NA",
+            gender: cust.gender || "NA",
+            dob: cust.dob || "NA",
+            activity_level: cust.activityLevel || "NA",
+            goal: cust.goal || "NA",
+            phone: cust.phone || "NA",
+            email: cust.email || "NA",
+            address: cust.address || "NA",
+            health_condition: cust.healthCondition || "NA",
+            occupation: cust.occupation || "NA",
+            availability: cust.availability || "NA",
+            commitment: cust.commitment || "NA"
         });
 
         // Save the customer in the database
         customer.save()
         .then(data => {
-            res.send(data);
+            resolve(data);
         }).catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the customer."
-            });
+            reject(err.message || "Some error occurred while creating the customer.");
         });
     });
 };
 
 // Update a customer identified by the customerId in the request
-exports.update = (req, res) => {
-    // Validate Request
-    if(!req.body.content) {
-        return res.status(400).send({
-            message: "Customer content can not be empty"
-        });
-    }
+exports.update = function(cust) {
+  return new Promise(function(resolve, reject) {
+        // Validate Request
+        if(!cust) {
+            reject("Customer content can not be empty");
+        }
 
-    // Find customer and update its name with the request body
-    Customer.findByIdAndUpdate(req.params.customerId, {
-        first_name: req.body.first_name || "NA",
-	last_name: req.body.last_name || "NA"
-    }, {new: true})
-    .then(customers => {
-        if(!customers) {
-            return res.status(404).send({
-                message: "Customer not found with id " + req.params.customerId
-            });
-        }
-        res.send(customers);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Customer not found with id " + req.params.customerId
-            });                
-        }
-        return res.status(500).send({
-            message: "Error updating customer with id " + req.params.customerId
+        // Find customer and update its name with the request body
+        Customer.findByIdAndUpdate(cust.customerId, {
+            first_name: cust.firstName || "NA",
+            last_name: cust.lastName || "NA"
+        }, {new: true})
+        .then(customers => {
+            if(!customers) {
+                reject("Customer not found with id " + cust.customerId);
+            }
+            resolve(customers);
+        }).catch(err => {
+            if(err.kind === 'ObjectId') {
+                reject("Customer not found with id " + cust.customerId);
+            }
+            reject("Error updating customer with id " + cust.customerId);
         });
     });
 };
 
 // Delete a customer with the specified customerId in the request
-exports.delete = (req, res) => {
-    Customer.findByIdAndRemove(req.params.customerId)
+exports.delete = function(customerId) {
+  return new Promise(function(resolve, reject) {
+    Customer.findByIdAndRemove(customerId)
     .then(customers => {
         if(!customers) {
-            return res.status(404).send({
-                message: "Customer not found with id " + req.params.customerId
-            });
+            reject("Customer not found with id " + customerId);
         }
-        res.send({message: "Customer deleted successfully!"});
+        resolve("Customer deleted successfully!");
     }).catch(err => {
-        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-            return res.status(404).send({
-                message: "Customer not found with id " + req.params.customerId
-            });                
-        }
-        return res.status(500).send({
-            message: "Could not delete customer with id " + req.params.customerId
+            if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+                reject("Customer not found with id " + customerId);            
+            }
+            reject("Could not delete customer with id " + customerId);
         });
     });
 };
