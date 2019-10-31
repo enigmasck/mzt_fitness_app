81e9902b7ff6b0a2fa6c333357d84ab436fbd4c1
Timothy Sanders <timothyasanders@gmail.com>
2019-10-31 20:07:53 +0100

    changed header to setHeader

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ var options = {
 };
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
