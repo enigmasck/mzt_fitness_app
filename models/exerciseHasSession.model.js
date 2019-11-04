20677472ec684b352ec62ff3c207a4a010badd9a
Zimeng HU <zimeng0223@gmail.com>
2019-11-04 13:22:07 +0100

    modify session service file to update correctly

--- models/exerciseHasSession.model.js
+++ models/exerciseHasSession.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ExerciseHasSessionSchema = mongoose.Schema({
 	exercise_id: String,
         session_id: String,
-        start_time: Date,   
+        start_time: Date,
         completed_time: Date
 }, {
 	timestamp: true
