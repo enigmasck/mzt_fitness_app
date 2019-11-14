a0e20411f5e95b64bb65f5a9fd4224439416faa2
Timothy Sanders <timothyasanders@gmail.com>
2019-11-14 18:58:51 +0100

    added file for notification strings

---
+++ message.strings/notification.strings.js
@@ -0,0 +1,14 @@
+/* 
+ * @desription: File for string literal constants for messages in the fitness app
+ *  This file is for notification messages, status_for, and status_type
+ */
+
+//status for
+const NOTIF_FOR_CUST = 'CUSTOMER';
+const NOTIF_FOR_COACH = 'COACH';
+
+//notify type
+const NOTIF_TYPE_FOCUS_SESSION_OPENED = "FOCUS_SESSION_OPENED";
+
+//notif
+const NOTIF_MSG_FOCUS_SESSION_OPENED = "Your coach has opened a new focus session for you.";
